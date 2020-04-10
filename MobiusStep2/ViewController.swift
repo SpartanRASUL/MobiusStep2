423517dec3ac8a1e6864386129a7af96d6497e63
Rasul Tataev <spartanrasul@gmail.com>
2020-04-10 16:56:13 +0300

    Initial Commit

---
+++ MobiusStep2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobiusStep2
+//
+//  Created by Rasul Tataev on 10/04/2020.
+//  Copyright © 2020 RSY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
